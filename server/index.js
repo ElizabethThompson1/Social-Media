87ba10cbab1e045c3b4d245e582a6217490352b9
Elizabeth <thompsondeveloper@yahoo.com>
2022-09-29 14:46:57 -0700

    added the mongoDB backend

--- server/index.js
+++ server/index.js
@@ -2,4 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
- 
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://Thompsonsma:thompson123@socialmedia.dzxxbk1.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {  useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running pn port : ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+
