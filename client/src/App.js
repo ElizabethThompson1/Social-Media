7f9ac8612ef3561907fb78c5e00f73238e38e8b2
Elizabeth <thompsondeveloper@yahoo.com>
2022-09-30 08:51:50 -0700

    created folders that will hold my layout for posts and my forms to make a post inside the both have there js and style on the mainpage App i implamented mui/material containers i them imported the files that will my my post anf form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import {Container, AppBar, Typography, Grow, Grid} from "@mui/material"
+import post from "./images/donuts.jpg";
 
 const App = () => {
     return(
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth ="lg">
+            <AppBar position="static" color="inherit">
+                <Typography varint="h2" align="center">Posts</Typography>
+                <img src={post} alt="post" />
+            </AppBar>
+        </Container>
     )
 }
 export default App
