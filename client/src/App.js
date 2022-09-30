1643c7f800aa5a3e5eeb06c84eb492a909f8bdee
Elizabeth <thompsondeveloper@yahoo.com>
2022-09-30 11:10:37 -0700

    created all layouts ans connected styles to them

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import {Container, AppBar, Typography, Grow, Grid} from "@mui/material"
-import post from "./images/donuts.jpg";
+import image from "./images/donuts.jpg";
+import Post from "./components/Posts/Posts";
+import Form from "./components/Forms/Form";
+
+
 
 const App = () => {
     return(
         <Container maxidth ="lg">
             <AppBar position="static" color="inherit">
                 <Typography varint="h2" align="center">Posts</Typography>
-                <img src={post} alt="post" />
+                <img src={image} alt="post" />
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Post/>
+                    </Grid>
+                    <Grid item xs ={12} sm={4}>
+                        <Form/>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     )
 }
